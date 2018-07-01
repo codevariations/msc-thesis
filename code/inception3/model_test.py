3f0feaa12da47ddc5c29434a2445727877d4cf72
HHalva <halva.hermanni@gmail.com>
2018-07-01 14:39:21 +0100

    training code

--- code/inception3/model_test.py
+++ code/inception3/model_test.py
@@ -1,5 +1,6 @@
 import torch
 import torchvision
+import time
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
@@ -36,7 +37,7 @@ dataloaders = {x: torch.utils.data.DataLoader(img_datasets[x],
 dataset_sizes = {x: len(img_datasets[x]) for x in ['train', 'val']}
 class_names = img_datasets['train'].classes
 
-#input some data
+#input example image and plot it
 img_in, imb_label = next(iter(dataloaders['train']))
 
 #plot it
@@ -50,6 +51,88 @@ def imshow(inp, title=None):
 out = torchvision.utils.make_grid(img_in)
 imshow(out)
 plt.show()
+
+#define function to train a model
+def train_model(model, criterion, optimizer, scheduler, n_epochs=25):
+    since = time.time()
+
+    best_model_ws = copy.deepcopy(model.state_dict())
+    best_top_acc = 0.0 
+    best_five_acc = 0.0 
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, n_epochs-1))
+        print('-'*10)
+
+        #training and validation per epoch
+        for stage in ['train', 'val']:
+            if stage == 'train':
+                scheduler.step()
+                model.train() #activate training mode
+            else:
+                model.eval() #activate validation mode
+            
+            run_loss = 0.0
+            run_correct = 0
+
+            #iterate over data
+            for inputs, labels in dataloaders[stage]:
+                optimizer.zero_grad()
+
+                #forward pass
+                with torch.set_grad_enabled(stage == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    #backwards pass
+                    if stage == 'train':
+                        loss.backward()
+                        optimizer.step()
+                 
+                run_loss += loss.item()*inputs.size(0)
+                run_correct += torch.sum(preds==labels.data)
+
+            epoch_loss = run_loss / dataset_sizes[stage]
+            epoch_acc = run_correct.double() / dataset_sizes[stage]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                stage, epoch_loss, epoch_acc))
+
+            #deep copy the model
+            if stage == 'val' and epoch_acc > best_top_acc:
+                best_acc = epoch_acc
+                best_model_ws = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since 
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    model.load_state_dict(best_model_ws)
+    return model
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
