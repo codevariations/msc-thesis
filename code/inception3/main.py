de460e48c836a058aa5d00d6b03399515b147182
HHalva <halva.hermanni@gmail.com>
2018-07-09 12:35:35 +0100

    switching to vgg

--- code/inception3/main.py
+++ code/inception3/main.py
@@ -1,5 +1,8 @@
-import argparse
 import os
+os.environ['CUDA_DEVICE_ORDER'] = "PCI_BUS_ID"
+os.environ['CUDA_VISIBLE_DEVICES'] = "2, 3"
+
+import argparse
 import random
 import shutil
 import time
@@ -17,18 +20,13 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
 
-model_names = sorted(name for name in models.__dict__
-    if name.islower() and not name.startswith("__")
-    and callable(models.__dict__[name]))
+import pdb
+from HEInception3 import *
+
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 parser.add_argument('data', metavar='DIR',
                     help='path to dataset')
-parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet18',
-                    choices=model_names,
-                    help='model architecture: ' +
-                        ' | '.join(model_names) +
-                        ' (default: resnet18)')
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
 parser.add_argument('--epochs', default=90, type=int, metavar='N',
@@ -86,16 +84,17 @@ def main():
     args.distributed = args.world_size > 1
 
     if args.distributed:
-        dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
+        dist.init_process_group(backend=args.dist_backend,
+                                init_method=args.dist_url,
                                 world_size=args.world_size)
 
     # create model
     if args.pretrained:
-        print("=> using pre-trained model '{}'".format(args.arch))
-        model = models.__dict__[args.arch](pretrained=True)
+        print("=> using pre-trained model")
+        model = he_inception_v3(pretrained=True)
     else:
-        print("=> creating model '{}'".format(args.arch))
-        model = models.__dict__[args.arch]()
+        print("=> traing from scratch")
+        model = he_inception_v3(pretrained=False)
 
     if args.gpu is not None:
         model = model.cuda(args.gpu)
@@ -103,11 +102,7 @@ def main():
         model.cuda()
         model = torch.nn.parallel.DistributedDataParallel(model)
     else:
-        if args.arch.startswith('alexnet') or args.arch.startswith('vgg'):
-            model.features = torch.nn.DataParallel(model.features)
-            model.cuda()
-        else:
-            model = torch.nn.DataParallel(model).cuda()
+        model = torch.nn.DataParallel(model).cuda()
 
     # define loss function (criterion) and optimizer
     criterion = nn.CrossEntropyLoss().cuda(args.gpu)
@@ -115,21 +110,21 @@ def main():
     optimizer = torch.optim.SGD(model.parameters(), args.lr,
                                 momentum=args.momentum,
                                 weight_decay=args.weight_decay)
-
-    # optionally resume from a checkpoint
-    if args.resume:
-        if os.path.isfile(args.resume):
-            print("=> loading checkpoint '{}'".format(args.resume))
-            checkpoint = torch.load(args.resume)
-            args.start_epoch = checkpoint['epoch']
-            best_prec1 = checkpoint['best_prec1']
-            model.load_state_dict(checkpoint['state_dict'])
-            optimizer.load_state_dict(checkpoint['optimizer'])
-            print("=> loaded checkpoint '{}' (epoch {})"
-                  .format(args.resume, checkpoint['epoch']))
-        else:
-            print("=> no checkpoint found at '{}'".format(args.resume))
-
+#
+#    # optionally resume from a checkpoint
+#    if args.resume:
+#        if os.path.isfile(args.resume):
+#            print("=> loading checkpoint '{}'".format(args.resume))
+#            checkpoint = torch.load(args.resume)
+#            args.start_epoch = checkpoint['epoch']
+#            best_prec1 = checkpoint['best_prec1']
+#            model.load_state_dict(checkpoint['state_dict'])
+#            optimizer.load_state_dict(checkpoint['optimizer'])
+#            print("=> loaded checkpoint '{}' (epoch {})"
+#                  .format(args.resume, checkpoint['epoch']))
+#        else:
+#            print("=> no checkpoint found at '{}'".format(args.resume))
+#
     cudnn.benchmark = True
 
     # Data loading code
@@ -141,14 +136,16 @@ def main():
     train_dataset = datasets.ImageFolder(
         traindir,
         transforms.Compose([
-            transforms.RandomResizedCrop(224),
+            transforms.RandomResizedCrop(299),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             normalize,
         ]))
 
     if args.distributed:
-        train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+        train_sampler = torch.utils.data.distributed.DistributedSampler(
+                      train_dataset
+                      )
     else:
         train_sampler = None
 
@@ -158,17 +155,17 @@ def main():
 
     val_loader = torch.utils.data.DataLoader(
         datasets.ImageFolder(valdir, transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
+            transforms.Resize(300),
+            transforms.CenterCrop(299),
             transforms.ToTensor(),
             normalize,
         ])),
         batch_size=args.batch_size, shuffle=False,
         num_workers=args.workers, pin_memory=True)
 
-    if args.evaluate:
-        validate(val_loader, model, criterion)
-        return
+#    if args.evaluate:
+#        validate(val_loader, model, criterion)
+#        return
 
     for epoch in range(args.start_epoch, args.epochs):
         if args.distributed:
@@ -178,19 +175,19 @@ def main():
         # train for one epoch
         train(train_loader, model, criterion, optimizer, epoch)
 
-        # evaluate on validation set
-        prec1 = validate(val_loader, model, criterion)
-
-        # remember best prec@1 and save checkpoint
-        is_best = prec1 > best_prec1
-        best_prec1 = max(prec1, best_prec1)
-        save_checkpoint({
-            'epoch': epoch + 1,
-            'arch': args.arch,
-            'state_dict': model.state_dict(),
-            'best_prec1': best_prec1,
-            'optimizer' : optimizer.state_dict(),
-        }, is_best)
+#        # evaluate on validation set
+#        prec1 = validate(val_loader, model, criterion)
+#
+#        # remember best prec@1 and save checkpoint
+#        is_best = prec1 > best_prec1
+#        best_prec1 = max(prec1, best_prec1)
+#        save_checkpoint({
+#            'epoch': epoch + 1,
+#            'arch': args.arch,
+#            'state_dict': model.state_dict(),
+#            'best_prec1': best_prec1,
+#            'optimizer' : optimizer.state_dict(),
+#        }, is_best)
 
 
 def train(train_loader, model, criterion, optimizer, epoch):
@@ -207,6 +204,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
     for i, (input, target) in enumerate(train_loader):
         # measure data loading time
         data_time.update(time.time() - end)
+        print(i, data_time.avg)
 
         if args.gpu is not None:
             input = input.cuda(args.gpu, non_blocking=True)
@@ -214,84 +212,84 @@ def train(train_loader, model, criterion, optimizer, epoch):
 
         # compute output
         output = model(input)
-        loss = criterion(output, target)
+#        loss = criterion(output, target)
 
-        # measure accuracy and record loss
-        prec1, prec5 = accuracy(output, target, topk=(1, 5))
-        losses.update(loss.item(), input.size(0))
-        top1.update(prec1[0], input.size(0))
-        top5.update(prec5[0], input.size(0))
+#        # measure accuracy and record loss
+#        prec1, prec5 = accuracy(output, target, topk=(1, 5))
+#        losses.update(loss.item(), input.size(0))
+#        top1.update(prec1[0], input.size(0))
+#        top5.update(prec5[0], input.size(0))
+#
+#        # compute gradient and do SGD step
+#        optimizer.zero_grad()
+#        loss.backward()
+#        optimizer.step()
+#
+#        # measure elapsed time
+#        batch_time.update(time.time() - end)
+#        end = time.time()
+#
+#        if i % args.print_freq == 0:
+#            print('Epoch: [{0}][{1}/{2}]\t'
+#                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+#                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+#                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+#                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+#                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+#                   epoch, i, len(train_loader), batch_time=batch_time,
+#                   data_time=data_time, loss=losses, top1=top1, top5=top5))
+#
 
-        # compute gradient and do SGD step
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        # measure elapsed time
-        batch_time.update(time.time() - end)
-        end = time.time()
-
-        if i % args.print_freq == 0:
-            print('Epoch: [{0}][{1}/{2}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                   epoch, i, len(train_loader), batch_time=batch_time,
-                   data_time=data_time, loss=losses, top1=top1, top5=top5))
-
-
-def validate(val_loader, model, criterion):
-    batch_time = AverageMeter()
-    losses = AverageMeter()
-    top1 = AverageMeter()
-    top5 = AverageMeter()
-
-    # switch to evaluate mode
-    model.eval()
-
-    with torch.no_grad():
-        end = time.time()
-        for i, (input, target) in enumerate(val_loader):
-            if args.gpu is not None:
-                input = input.cuda(args.gpu, non_blocking=True)
-            target = target.cuda(args.gpu, non_blocking=True)
-
-            # compute output
-            output = model(input)
-            loss = criterion(output, target)
-
-            # measure accuracy and record loss
-            prec1, prec5 = accuracy(output, target, topk=(1, 5))
-            losses.update(loss.item(), input.size(0))
-            top1.update(prec1[0], input.size(0))
-            top5.update(prec5[0], input.size(0))
-
-            # measure elapsed time
-            batch_time.update(time.time() - end)
-            end = time.time()
-
-            if i % args.print_freq == 0:
-                print('Test: [{0}/{1}]\t'
-                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                      'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                      'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                       i, len(val_loader), batch_time=batch_time, loss=losses,
-                       top1=top1, top5=top5))
-
-        print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'
-              .format(top1=top1, top5=top5))
-
-    return top1.avg
-
-
-def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
-    torch.save(state, filename)
-    if is_best:
-        shutil.copyfile(filename, 'model_best.pth.tar')
-
+#def validate(val_loader, model, criterion):
+#    batch_time = AverageMeter()
+#    losses = AverageMeter()
+#    top1 = AverageMeter()
+#    top5 = AverageMeter()
+#
+#    # switch to evaluate mode
+#    model.eval()
+#
+#    with torch.no_grad():
+#        end = time.time()
+#        for i, (input, target) in enumerate(val_loader):
+#            if args.gpu is not None:
+#                input = input.cuda(args.gpu, non_blocking=True)
+#            target = target.cuda(args.gpu, non_blocking=True)
+#
+#            # compute output
+#            output = model(input)
+#            loss = criterion(output, target)
+#
+#            # measure accuracy and record loss
+#            prec1, prec5 = accuracy(output, target, topk=(1, 5))
+#            losses.update(loss.item(), input.size(0))
+#            top1.update(prec1[0], input.size(0))
+#            top5.update(prec5[0], input.size(0))
+#
+#            # measure elapsed time
+#            batch_time.update(time.time() - end)
+#            end = time.time()
+#
+#            if i % args.print_freq == 0:
+#                print('Test: [{0}/{1}]\t'
+#                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+#                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+#                      'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+#                      'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+#                       i, len(val_loader), batch_time=batch_time, loss=losses,
+#                       top1=top1, top5=top5))
+#
+#        print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'
+#              .format(top1=top1, top5=top5))
+#
+#    return top1.avg
+#
+#
+#def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+#    torch.save(state, filename)
+#    if is_best:
+#        shutil.copyfile(filename, 'model_best.pth.tar')
+#
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
@@ -318,22 +316,22 @@ def adjust_learning_rate(optimizer, epoch):
         param_group['lr'] = lr
 
 
-def accuracy(output, target, topk=(1,)):
-    """Computes the precision@k for the specified values of k"""
-    with torch.no_grad():
-        maxk = max(topk)
-        batch_size = target.size(0)
-
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-        res = []
-        for k in topk:
-            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
-            res.append(correct_k.mul_(100.0 / batch_size))
-        return res
-
-
+#def accuracy(output, target, topk=(1,)):
+#    """Computes the precision@k for the specified values of k"""
+#    with torch.no_grad():
+#        maxk = max(topk)
+#        batch_size = target.size(0)
+#
+#        _, pred = output.topk(maxk, 1, True, True)
+#        pred = pred.t()
+#        correct = pred.eq(target.view(1, -1).expand_as(pred))
+#
+#        res = []
+#        for k in topk:
+#            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+#            res.append(correct_k.mul_(100.0 / batch_size))
+#        return res
+#
+#
 if __name__ == '__main__':
-    main()
+   main()
